d33f87cc3e6ec05f2cb37d412c43394fd3652f0a
AJ <105689704+ajdev05@users.noreply.github.com>
2024-04-23 18:44:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import cv2
 from djitellopy import tello
 import cvzone
+import time
 
-thres = 0.6
-nmsThres = 0.2
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
 
+drone = tello.Tello()
+drone.connect()
+
+imgthold = 0.6
+nmsthold = 0.2
 classNames = []
 classFile = "coco.names"
 with open(classFile, 'rt') as file:
     classNames = file.read().split('\n')
 
 configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = 'frozen_inference_graph.pb'
+weightPath = 'frozen_inference_graph.pb'
 
-net = cv2.dnn_DetectionModel(configPath,weightsPath)
+net = cv2.dnn_DetectionModel(configPath,weightPath)
 
 net.setInputSize(320,320)
 net.setInputScale(1.0/ 120)
@@ -25,16 +26,32 @@ net.setInputSwapRB(True)
 
 
 
-while True:
-    success, img = cap.read()
-    classIds, confs, bbox = net.detect(img,confThreshold=thres, nmsThreshold=nmsThres)
+print(f"Battery: {drone.get_battery()}%")
+time.sleep(2)
 
+drone.streamoff()
+
+drone.streamon()
+
+
+
+
+
+while True:
+    img = drone.get_frame_read().frame
+
+    classIds, confs, bbox = net.detect(img,confThreshold=imgthold, nmsThreshold=nmsthold)
     try:
         for classId, conf, box in zip(classIds.flatten(),confs.flatten(), bbox):
             print(classId,conf, box)
+            cvzone.cornerRect(img, box, rt=0)
+
+            cv2.putText(img, f"{classNames[classId-1].upper()} {round(conf*100,2)}%", (box[0]+10, box[1]+30), cv2.FONT_HERSHEY_COMPLEX, 1,(251, 255, 0),2)
     except:
         pass
 
-    cv2.imshow("Image", img)
-    key = cv2.waitKey(1)
+    drone.send_rc_control(0,0,0,0)
 
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imshow("Drone Image Detection", img_rgb)
+    key = cv2.waitKey(1)
